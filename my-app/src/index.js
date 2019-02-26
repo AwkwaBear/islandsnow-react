456bab88e6cbbaa0ace5bd9b677f7b1ec248914b
howtotailslide <agasbarro36@hotmail.com>
2019-02-25 23:41:40 -1000

    Task 4

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Dropdown, Icon, Menu, Image, List, Divider, GridColumn } from 'semantic-ui-react';
+import { Container, Dropdown, Icon, Menu, Image, List, Divider, Grid, Input } from 'semantic-ui-react';
 
 class TopMenu extends React.Component {
   render() {
@@ -101,27 +101,36 @@ class FooterMenu extends React.Component {
   render() {
     return (
         <Container three column grid classname="footer">
-          <GridColumn>
-            <List>
-              Navigation
-              <Divider/>
-              <List.Item>About Us</List.Item>
-              <List.Item>Videos</List.Item>
-              <List.Item>Store Location</List.Item>
-            </List>
-          </GridColumn>
-          <GridColumn>
-            <List>
-              Main Menu
-              <Divider/>
-              <List.Item>Men</List.Item>
-              <List.Item>Women</List.Item>
-              <List.Item>Kids</List.Item>
-            </List>
-          </GridColumn>
-          <GridColumn>
-            
-          </GridColumn>
+          <Grid columns="three">
+            <Grid.Row>
+              <Grid.Column>
+                <List>
+                  Navigation
+                  <Divider/>
+                  <List.Item>About Us</List.Item>
+                  <List.Item>Videos</List.Item>
+                  <List.Item>Store Location</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                <List>
+                  Main Menu
+                  <Divider/>
+                  <List.Item>Mens</List.Item>
+                  <List.Item>Women</List.Item>
+                  <List.Item>Kids</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                <List>
+                  Connect
+                  <Divider/>
+                  <List.Item>Sign Up for the Latest Updates</List.Item>
+                  <List.Item><Input action="Join" placeholder="Enter Email Address"/></List.Item>
+                </List>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Container>
     )
   }
